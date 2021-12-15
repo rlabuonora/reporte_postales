20f287c374affb972765d464128dcad1b3f87841
Rafael La Buonora <info@rlabuonora.com>
2021-12-15 17:21:20 -0300

    Avanza hasta tarifas

---
+++ importar_datos.R
@@ -0,0 +1,10 @@
+library(galeriaR)
+
+# Cartas
+cartas_base_larga <- importar_cartas_base_larga()
+saveRDS(cartas_base_larga, here::here("data", "cartas_base_larga.rds"))
+
+
+# Paquetes
+paquetes_base_larga <- importar_paquetes_base_larga()
+saveRDS(paquetes_base_larga, here::here("data", "paquetes_base_larga.rds"))
