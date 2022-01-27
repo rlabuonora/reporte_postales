74827389ca8597fcd2c54f91923b65b98da69dbf
Rafael La Buonora <info@rlabuonora.com>
2022-01-27 14:09:02 -0300

    Graficos final

--- importar_datos.R
+++ importar_datos.R
@@ -8,3 +8,10 @@ saveRDS(cartas_base_larga, here::here("data", "cartas_base_larga.rds"))
 # Paquetes
 paquetes_base_larga <- importar_paquetes_base_larga()
 saveRDS(paquetes_base_larga, here::here("data", "paquetes_base_larga.rds"))
+
+
+# Giros
+giros_base_larga <- importar_giros_base_larga()
+saveRDS(giros_base_larga, here::here("data", "giros_base_larga.rds"))
+
+
